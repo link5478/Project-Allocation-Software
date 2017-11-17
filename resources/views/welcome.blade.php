90c31da4a693975dd7dbd5f9ece34033f1b83dec
link5478 <cheyne.carsten@gmail.com>
2017-11-17 00:54:15 +0000

    Modified file to test lock.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Yo Dawg
                 </div>
 
                 <div class="links">
