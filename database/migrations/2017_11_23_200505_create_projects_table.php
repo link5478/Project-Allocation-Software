a2c4417e0dc5f3ef0aed4d90ee66fac85b86e3e7
link5478 <cheyne.carsten@gmail.com>
2017-11-25 20:32:54 +0000

    Updated schema to include hidden attribute

--- database/migrations/2017_11_23_200505_create_projects_table.php
+++ database/migrations/2017_11_23_200505_create_projects_table.php
@@ -18,6 +18,7 @@ class CreateProjectsTable extends Migration
             $table->string('name');
             $table->string('description');
             $table->integer('availability');
+            $table->tinyInteger('hidden')->default(0);
             $table->integer('supervisor_ID');
         });
     }
