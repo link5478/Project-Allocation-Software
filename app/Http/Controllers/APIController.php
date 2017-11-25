e3f2d90a5ef8881196cc17209e75a55ead28127e
link5478 <cheyne.carsten@gmail.com>
2017-11-25 20:33:22 +0000

    Added API calls to retrieve all projects for a supervisor, and let them add one

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -11,37 +11,41 @@ namespace App\Http\Controllers;
 
 class APIController
 {
-    //Example from Industrial Team Project
 
-//    /**
-//     * Return the total number of unique customers that performed transactions during a given period
-//     *
-//     * /api/stores/Spare/unique-customers/2015-08-01/2015-08-30
-//     *
-//     * @param Request $request
-//     * @return $this
-//     */
-//    public function uniqueCustomers(Request $request)
-//    {
-//        $uniqueCustomers = Store::where('outlet_name', '=', $request->store_name)
-//            ->get()
-//            ->map(function ($item) use ($request) {
-//                return [
-//                    'store_name' => $item['outlet_name'],
-//                    'store_colour' => Colours::where('store', '=',
-//                        $item['outlet_name'])->pluck('chart_colour')->first(),
-//                    'unique_customers' => Transaction::where('date', '>=', $request->period1)
-//                        ->where('date', '<=', $request->period2)
-//                        ->where('outlet_name', '=', $request->store_name)
-//                        ->pluck('customer_id')
-//                        ->unique()
-//                        ->count()
-//                ];
-//            });
-//        return response()
-//            ->json($uniqueCustomers)
-//            ->header(self::CORS_KEY, self::CORS_VALUE);
-//    }
+    /**
+     * Returns a list of projects assigned to a supervisor
+     *
+     * /api/projects/
+     *
+     * @param Request $request
+     * @return $this
+     */
+    public function supervisorGetProjects(Request $request)
+    {
+        return DB::table('projects')
+            ->select('id')
+            ->select('name')
+            ->where('supervisor_ID', '=', $request->supervisorID)
+            ->get();
+    }
+
+    /**
+     * Inserts a Project entry into the database
+     *
+     * /api/projects/add
+     *
+     * @param Request $request
+     * @return $this
+     */
+    public function supervisorAddProject(Request $request)
+    {
+        DB::table('projects')->insert(
+            ['name' => $request->name,
+                'description' =>$request->desc,
+                'availability' => $request->available,
+                'supervisor_ID' => $request->supervisorID]
+        );
+    }
 
 
 
