1f59d08de0afc89dfef8650dac7e13f4f1ec97f2
link5478 <cheyne.carsten@gmail.com>
2017-11-30 02:40:46 +0000

    added to display project details. good use of OOP

---
+++ resources/assets/js/components/supervisor_project.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="panel panel-default">
+                <div class="panel-heading">Example Component</div>
+
+                <div class="panel-body">
+                    <p>Name: {{name}}</p>
+                    <button>Edit</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+
+        props:['name'],
+
+
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
