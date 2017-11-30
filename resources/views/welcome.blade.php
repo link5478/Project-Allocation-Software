4c4c5d068d37c4057a2ac3354c839dabd4120516
link5478 <cheyne.carsten@gmail.com>
2017-11-30 08:54:08 +0000

    Changed Title for page, and added link to Projects page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -78,7 +78,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Yo Dawg
+                    Project Allocation Software
                 </div>
 
                 <div class="links">
@@ -87,6 +87,8 @@
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
+
+                    <a href="{{route('supervisor.projects')}}">My Projects</a>
                 </div>
             </div>
         </div>
