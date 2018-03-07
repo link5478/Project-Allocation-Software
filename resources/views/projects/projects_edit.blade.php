0e396d2e4dba4536dd28f5d65d56ea869021c35e
Carsten Cheyne <cheyne.carsten@gmail.com>
2018-03-07 05:06:18 +0000

    Front end for editing pages.

---
+++ resources/views/projects/projects_edit.blade.php
@@ -0,0 +1,77 @@
+@extends('layouts.app')
+
+@section('title', 'Edit Your Project')
+
+@section('content')
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>@yield('title')</h1>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-12">
+
+                @if(count($errors))
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+
+                @if (Session::has('message'))
+                    <div class="alert alert-info">{{ Session::get('message') }}</div>
+                @endif
+
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <form action="{{ route('projects.update', $project->id) }}" method="POST" enctype="multipart/form-data">
+                    {{ csrf_field() }}
+                    {{ method_field('PUT') }}
+
+                    <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                        <label for="name">Name</label>
+                        <input type="text" class="form-control" id="name" name="name"
+                               placeholder="{{$project->name}}"
+                               value="{{ Request::old('name', $project->name) }}">
+                    </div>
+
+                    <div class="form-group{{ $errors->has('description') ? ' has-error' : '' }}">
+                        <label for="description">Description</label>
+                        <input type="text" class="form-control" id="description" name="description"
+                               placeholder="{{$project->description}}"
+                               value="{{ Request::old('description', $project->description) }}">
+                    </div>
+
+
+                    <div class="form-group{{ $errors->has('hidden') ? ' has-error' : '' }}">
+                        <label for="hidden">Hidden</label>
+                        <input type="text" class="form-control" id="hidden" name="hidden"
+                               placeholder="{{$project->hidden}}"
+                               value="{{ Request::old('hidden', $project->hidden) }}">
+                    </div>
+
+                    <div class="form-group{{ $errors->has('availability') ? ' has-error' : '' }}">
+                        <label for="availability">Availability</label>
+                        <input type="text" class="form-control" id="availability" name="availability"
+                               placeholder="{{$project->availability}}"
+                               value="{{ Request::old('availability', $project->availability) }}">
+                    </div>
+
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary">Update Project</button>
+                    </div>
+
+                </form>
+            </div>
+        </div>
+    </div>
+
+@endsection
