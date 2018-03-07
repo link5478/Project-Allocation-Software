bb2a95d4b1e5f67295c7dd4158b6b1ddbf7b7495
Carsten Cheyne <cheyne.carsten@gmail.com>
2018-03-07 05:03:52 +0000

    Unncessary - Removed

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -10,17 +10,5 @@ use App\Http\Controllers\APIController;
 
 class DataController extends Controller
 {
-    public function supervisorMyProjects()
-    {
-        $id = Auth::id();
-        $api = new APIController();
-        $data = $api->supervisorGetProjectsLocal($id);
-        return view('supervisor.myprojects')->with('data', $data);
-    }
-
-    public function supervisorAddProjects()
-    {
-        return view('supervisor.addProjects');
-    }
 
 }
