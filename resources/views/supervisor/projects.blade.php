e0f57d69dd0c1b80d35640d84fe90b0d96ac6015
Carsten Cheyne <cheyne.carsten@gmail.com>
2018-03-08 04:21:51 +0000

    Displays all relevant supervisor projects.

---
+++ resources/views/supervisor/projects.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+
+        <div class="row">
+            <div class="col-md-12">
+
+                @if(count($errors))
+                    <div class="alert alert-danger ">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+
+                @if (Session::has('message'))
+                    <div class="alert alert-info alert-dismissible">
+                        {{ Session::get('message') }}
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                @endif
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-primary">
+                    <div class="panel-heading" align="center">My Projects</div>
+                    <div class="panel panel-default">
+                        <div class="panel-heading" align="center">Want to add a new project? <a href="{{route('supervisor.projects.add')}}">Add</a></div>
+                        @foreach ($data as $d)
+                            <div class="panel-body">
+                                <a href="{{route('project', $d->id)}}"> {{$d->name}} </a>
+                                <a href="{{route('supervisor.projects.edit', $d->id)}}">Edit</a>
+                                <a href="{{route('supervisor.projects.archive', $d->id)}}">Archive</a>
+                                <a href="{{route('supervisor.projects.clone', $d->id)}}">Clone</a>
+                            </div>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
