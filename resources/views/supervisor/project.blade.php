ea4a4936f4f5591684cef8d5cc95a27ea0cf8ed5
Carsten Cheyne <cheyne.carsten@gmail.com>
2018-03-08 04:21:29 +0000

    Display for individual supervsor projects.

---
+++ resources/views/supervisor/project.blade.php
@@ -0,0 +1,50 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+
+        <div class="row">
+            <div class="col-md-12">
+
+                @if(count($errors))
+                    <div class="alert alert-danger ">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+
+                @if (Session::has('message'))
+                    <div class="alert alert-info alert-dismissible">
+                        {{ Session::get('message') }}
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                @endif
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-primary">
+                    <div class="panel-heading" align="center">{{$data['name']}}</div>
+                    <div class="panel panel-default">
+                        <div class="panel-heading" align="center">Description</div>
+                        <div class="panel-body" align="center">{{$data['description']}} </div>
+                    </div>
+                    <div class="panel panel-default">
+                        <div class="panel-heading" align="center">Availability</div>
+                        <div class="panel-body" align="center">{{$data['availability']}}</div>
+                    </div>
+                    <div class="panel panel-default">
+                        <div class="panel-heading" align="center">Supervisor In Charge</div>
+                        <div class="panel-body" align="center">{{$data['supervisor_name']}}</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
