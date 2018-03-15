4f463401aeba9200dfb2ae404943280041959909
Carsten Cheyne <cheyne.carsten@gmail.com>
2018-03-15 03:41:25 +0000

    Fixed to display student functionality such as choices and interests

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 
 use App\ArchivedProject;
+use App\Interest;
+use Illuminate\Contracts\Session\Session;
 use Illuminate\Http\Request;
 use App\Project;
 use Illuminate\Support\Carbon;
@@ -11,6 +13,8 @@ use Illuminate\Support\Facades\Auth;
 use App\User;
 use App\Choices;
 use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Schema;
+use function Symfony\Component\VarDumper\Dumper\esc;
 
 class StudentController extends Controller
 {
@@ -18,34 +22,27 @@ class StudentController extends Controller
     public function index()
     {
         $supervisors = User::all('id', 'name', 'is_supervisor')->where('is_supervisor','=', '1');
-        $choice = $this->currentUserChoice();
         $data = [];
         foreach($supervisors as $s)
         {
-            $projects  = Project::all('id', 'name', 'description', 'supervisor_id')->where('supervisor_id', '=', $s->id);
-            $data["'".$s->name."'"] = [];
+            $projects  = Project::all('id', 'name', 'description', 'supervisor_id')->where('supervisor_id', '=', $s->id)->where('hidden', '=', 0);
+            $data[$s->name] = [];
             foreach($projects as $p)
             {
-                $is_choice = 'No';
-
-                if($choice->project1 == $p->id)
+                $interest = Interest::all()->where('student_id', '=', Auth::id())->where('project_id', '=', $p->id)->first();
+                $is_interested = false;
+                // if we have an interest entry, we want to look at it.
+                if($interest != null)
                 {
-                    $is_choice = 'No';
+                    if($interest->project_id == $p->id)
+                    {
+                        $is_interested = true;
+                    }
                 }
-                else if($choice->project2 == $p->id)
-                {
-                    $is_choice = 'No';
-                }
-                else if($choice->project3 == $p->id)
-                {
-                    $is_choice = 'No';
-                }
-                $project = ['project_id' => $p->id, 'name'=> $p->name, 'description' => $p->description, 'is_choice' => $is_choice];
-                array_push($data["'".$s->name."'"], $project);
+                $project = ['project_id' => $p->id, 'name'=> $p->name, 'description' => $p->description, 'interested' => $is_interested];
+                array_push($data[$s->name], $project);
             }
         }
-
-
         return view ('student.projects')->with('data', $data);
 
     }
@@ -77,5 +74,73 @@ class StudentController extends Controller
             return $Choices;
         }
         return $current;
+    }
+
+    public function addInterest($id)
+    {
+        $interested = Interest::all()->where('project_id', '=', $id)->where('student_id', '=',Auth::id())->first();
+        if($interested == null)
+        {
+            $interested = new Interest();
+            $interested->project_id = $id;
+            $interested->student_id = Auth::id();
+            $interested->created_at = Carbon::now();
+            $interested->updated_at = Carbon::now();
+            $interested->save();
+            return Redirect::back();
+        }
+        else
+        {
+            $interested->delete();
+            return Redirect::back();
+        }
+    }
+
+    public function viewChoices()
+    {
+        // should only be one choice really.
+        $choices = Choices::all()->where('student_id', '=', auth::id())->first();
+        $projects = Project::all('id', 'name', 'hidden')->where('hidden', '=', 0);
+
+        return view('student.choices')->with('choice', $choices)->with('projects', $projects);
+    }
+
+    public function update(Request $request, Choices $choice)
+    {
+        if(Auth::id() != $choice->student_id)
+        {
+            return redirect('home');
+        }
+
+        if (!empty($request->input('choice1')))
+        {
+            $choice->project1 = $request->input('choice1');
+        }
+        if (!empty($request->input('choice2')))
+        {
+            $choice->project2 = $request->input('choice2');
+        }
+        if (!empty($request->input('choice3'))) {
+            $choice->project3 = $request->input('choice3');
+        }
+
+        if (!empty($request->input('additional_info'))) {
+            $choice->additional_info = $request->input('additional_info');
+        }
+        $choice->save();
+        return Redirect::back()->with('message', 'Operation Successful !');
+    }
+
+    public function interested_toggle(Request $request)
+    {
+        if($request->session()->has('interested'))
+        {
+            $request->session()->put('interested', !$request->session()->get('interested'));
+        }
+        else
+        {
+            $request->session()->put('interested', true);
+        }
+        return Redirect::back();
     }
 }
