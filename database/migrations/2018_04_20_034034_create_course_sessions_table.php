5cd3449c2cba632546cb511092d7a890a1fe5f4a
Carsten Cheyne <cheyne.carsten@gmail.com>
2018-04-29 01:06:30 +0100

    Fixed potential Travis CI error.

--- database/migrations/2018_04_20_034034_create_course_sessions_table.php
+++ database/migrations/2018_04_20_034034_create_course_sessions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateSessionsTable extends Migration
+class CreateCourseSessionsTable extends Migration
 {
     /**
      * Run the migrations.
