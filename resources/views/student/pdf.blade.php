3196e20f114c6ae53f5affa684df3c84b1982dca
Carsten Cheyne <cheyne.carsten@gmail.com>
2018-04-29 22:16:27 +0100

    Added a pdf blade to be mirrored onto a pdf doc

---
+++ resources/views/student/pdf.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title></title>
+</head>
+
+<style>
+    p.wrap {
+        word-wrap: break-word;
+    }
+</style>
+
+
+<body>
+
+    <h2>
+        <u>{{$info['session']}}</u> <br>
+        <u>Project List</u>
+
+    </h2>
+
+    <br>
+
+    @php $projects = $info['projects'] @endphp
+
+    @foreach ($projects as $proj)
+
+        <h3>
+            <u>{{$proj['name']}}</u>
+        </h3>
+        <h4>
+            Supervisor: {{$proj['supervisor_name']}} <br>
+            Email Address:
+        </h4>
+        <p class = "wrap">
+            {{$proj['description']}}
+
+        </p>
+        <br>
+        @endforeach
+
+
+
+
+
+
+
+</body>
+</html>
