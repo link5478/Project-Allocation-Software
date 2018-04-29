50b05fece0f6088ed224b8863605d2a2b7af228b
Carsten Cheyne <cheyne.carsten@gmail.com>
2018-04-29 01:52:53 +0100

    Fixed UI changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,52 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Dashboard</div>
 
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                    <div class="panel-body">
+                        @if (session('status'))
+                            <div class="alert alert-success">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+                        <p>
+                            @if(\App\courseSession::GetSession())
+                                Current Session: {{App\courseSession::GetSession()->name}}<br>
+                                Start Date: {{Carbon\Carbon::parse(App\courseSession::GetSession()->start)->toDateString()}}<br>
+                                End Date: {{Carbon\Carbon::parse(App\courseSession::GetSession()->end)->toDateString()}}<br>
+                            @endif
+                        <br>
+                        You are a:
+                        @auth
+                            @if(Auth::user()->is_supervisor == 1)
+                                <br> <u>SUPERVISOR</u>
+                                <br> Projects that you wish to oversee this session can be created and modified under "My Projects"
+                                <br> You may archive projects, or retrieve archived projects from "Archived Projects"
 
-                    You are logged in!
+                            @endif
+                            <br>
+                            @if(Auth::user()->is_student == 1)
+                                <br> <u>STUDENT</u>
+                                <br> You can select which projects you would like to do which can be found under "My Choices"
+                                <br> A list of all projects can be found under the "View Projects" header
+
+                            @endif
+                            <br>
+                            @if(Auth::user()->is_coordinator == 1)
+                                <br> <u>PROJECT COORDINATOR</u>
+                                <br> A project session can be created under "Session". A session must be created in order for students to make their project choices
+                                <br> Additionally students will have to be added to a session manually
+                                <br> Allocating projects to students can be done using the Allocation tool found under "Allocation"
+
+                            @endif
+                        @endauth
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,101 +1,96 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-</head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
+        <!-- Styles -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    </head>
+    <body>
+        <div id="app">
+            <nav class="navbar navbar-default navbar-static-top">
+                <div class="container">
+                    <div class="navbar-header">
 
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
+                        <!-- Collapsed Hamburger -->
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                            <span class="sr-only">Toggle Navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
 
-                    <!-- Branding Image -->
-                    <a href="{{ url('/home') }}">
-                        <img src = "https://imgur.com/74djVYg.png" alt="University of Dundee P.A.S" width="80" height="80">
-                    </a>
-                </div>
+                        <!-- Branding Image -->
+                        <a href="{{ url('/home') }}">
+                            <img src = "https://imgur.com/74djVYg.png" alt="University of Dundee P.A.S" width="80" height="80">
+                        </a>
+                    </div>
 
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
+                    <p class="navbar-text">{{App\courseSession::GetSession()->name}}</p>
 
+                    <div class="collapse navbar-collapse" id="app-navbar-collapse">
+                        <!-- Left Side Of Navbar -->
+                        <ul class="nav navbar-nav">
 
+                            <li><a href="{{ route('home') }}">Home</a></li>
 
-                        @auth
-                            @if(Auth::user()->is_supervisor == 1)
-                                @if(App\courseSession::GetSession() != null)
-                                    <li><a href="">{{App\courseSession::GetSession()->name}}</a></li>
+                            @auth
+                                @if(Auth::user()->is_supervisor == 1)
+                                    <li><a href="{{ route('supervisor.projects') }}">My Projects</a></li>
+                                    <li><a href="{{ route('archive.projects') }}">Archived Projects</a></li>
                                 @endif
-
-                                <li><a href="{{ route('supervisor.projects') }}">My Projects</a></li>
-                                <li><a href="{{ route('archive.projects') }}">Archived Projects</a></li>
-                            @endif
-                            @if(Auth::user()->is_student == 1)
-                                <li><a href="{{ route('student.projects') }}">View Projects</a></li>
-                                <li><a href="{{ route('student.choices') }}">My Choices</a></li>
-                            @endif
-                            @if(Auth::user()->is_coordinator == 1)
+                                @if(Auth::user()->is_student == 1)
+                                    <li><a href="{{ route('student.projects') }}">View Projects</a></li>
+                                    <li><a href="{{ route('student.choices') }}">My Choices</a></li>
+                                @endif
+                                @if(Auth::user()->is_coordinator == 1)
                                     <li><a href="{{ route('coordinator.sessions') }}">Session</a></li>
                                     <li><a href="#">Allocation</a></li>
-                            @endif
-                        @endauth
-                    </ul>
+                                @endif
+                            @endauth
+                        </ul>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                        @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    {{ Auth::user()->fname }} <span class="caret"></span>
-                                </a>
+                        <!-- Right Side Of Navbar -->
+                        <ul class="nav navbar-nav navbar-right">
+                            <!-- Authentication Links -->
+                            @guest
+                                <li><a href="{{ route('login') }}">Login</a></li>
+                            @else
+                                <li class="dropdown">
+                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                        {{ Auth::user()->fname }} <span class="caret"></span>
+                                    </a>
 
-                                <ul class="dropdown-menu" role="menu">
-                                    <li>
-                                        <a href="{{ route('home') }}">Home</a>
-                                    </li>
-                                    <li>
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        @endguest
-                    </ul>
+                                    <ul class="dropdown-menu" role="menu">
+                                        <li>
+                                            <a href="{{ route('logout') }}"
+                                               onclick="event.preventDefault();
+                                                             document.getElementById('logout-form').submit();">
+                                                Logout
+                                            </a>
+                                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                                {{ csrf_field() }}
+                                            </form>
+                                        </li>
+                                    </ul>
+                                </li>
+                            @endguest
+                        </ul>
+                    </div>
                 </div>
-            </div>
-        </nav>
+            </nav>
 
-        @yield('content')
-    </div>
+            @yield('content')
+        </div>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
-</body>
+    </body>
 </html>
