92dd9b001e83b7ef60a30647d20e8209697c9e6a
Carsten Cheyne <cheyne.carsten@gmail.com>
2018-04-29 22:16:11 +0100

    Changed title of website

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,8 +8,7 @@
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
-
+        <title>UoD P.A.S</title>
         <!-- Styles -->
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
